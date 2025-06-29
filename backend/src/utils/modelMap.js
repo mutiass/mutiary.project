40592913f01bec0ad753a45188812aca80498c5b
mutiass <158068300+mutiass@users.noreply.github.com>
2025-06-29 11:52:31 +0800

    Create utils/modelMap.js

---
+++ backend/src/utils/modelMap.js
@@ -0,0 +1,10 @@
+// utils/modelMap.js
+import Admin from '../models/adminModel.js';
+import Student from '../models/studentModel.js';
+import Teacher from '../models/teacherModel.js';
+
+export const modelMap = {
+  admin: Admin,
+  student: Student,
+  teacher: Teacher,
+};
